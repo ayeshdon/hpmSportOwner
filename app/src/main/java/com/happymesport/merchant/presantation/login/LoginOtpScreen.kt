719a8b8f1d58b636897c5df448dba43abf6024e0
ayeshdon <ayesh.don@gmail.com>
2025-05-28 23:08:17 +0800

    Update LoginOtpScreen.kt
    test

--- app/src/main/java/com/happymesport/merchant/presantation/login/LoginOtpScreen.kt
+++ app/src/main/java/com/happymesport/merchant/presantation/login/LoginOtpScreen.kt
@@ -124,6 +124,7 @@ fun signInOtpScreen(
         }
 
         Spacer(modifier = Modifier.height(24.dp))
+        
         LoginRoundedButton(
             enable = enteredOtp.length == 6,
             onClick = {
